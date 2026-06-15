8144cc1134c46da2f4822bd9dd25e2c8eea0a9ca
flexult <touma.leandre.72@gmail.com>
2026-06-15 09:42:55 +0000

    affichage des données (1)

--- js/seeDatas.js
+++ js/seeDatas.js
@@ -30,44 +30,73 @@ function createPopup (content) {
     return popup;
 }
 
+function createBox(elements) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    elements.forEach(element => {
+        box.innerText += element // TODO : sécuriser contre le XSS
+    });
+    return box;
+}
+
+/**
+ * @param {string} url - L'url à qui faire la requête
+ * @param {function} callback - Un callback à executer avec les données (dans la variable data)
+ */
+function requestApi (url, callback = ()=>{}) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            callback(data);
+            return true;
+        } else {
+            const error = data.error ?? "L'api n'a pas spécifié l'erreur";
+            alert("Erreur lors de la requete: " + error);
+            return false;
+        }
+    })
+    .catch (error => {
+        alert('Erreur lors de la requete: ' + error.message);
+        return false;
+    });
+}
+
 //EventListener
 backBtn.addEventListener('click', () => {
     window.location.assign("/menu.php")
 });
 
 leadersWinrateBtn.addEventListener('click', () => {
-    const popup = createPopup(['Classement des leaders par winrate:', 'Chargement des données...']);
+    const waitingText = document.createElement('p');
+    waitingText.innerText = 'Chargement des données...';
+    const popup = createPopup(['Classement des leaders par winrate:', waitingText]);
     document.body.append(popup);
-    fetch('/api.php?action=get_leaders_winrate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
+    requestApi('/api.php?action=get_leaders_winrate', (data) => {
+        waitingText.remove();
+        for (i = 0 ; i < data.length ; i++) {
+            const box = createBox(['leader_name: ', data[i]]);
+            popup.append(box);
         }
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert(JSON.stringify(data, null, 2));
-    })
-    .catch(error => {
-        alert(`Erreur lors du chargement des leaders: ${error.message}`);
     });
 });
 
 playersWinrateBtn.addEventListener('click', () => {
-    const popup = createPopup(['Winrate des joueurs:', 'Chargement des données...'])
+    const waitingText = document.createElement('p');
+    waitingText.innerText = 'Chargement des données...';
+    const popup = createPopup(['Winrate des joueurs:', waitingText])
     document.body.append(popup);
-    fetch('/api.php?action=get_players_winrate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
+    requestApi('/api.php?action=get_players_winrate', (data) => {
+        waitingText.remove();
+        for (i = 0 ; i < data.length ; i++) {
+            const box = createBox(['player_name: ', data[i]]);
+            popup.append(box);
         }
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert(JSON.stringify(data, null, 2));
-    })
-    .catch(error => {
-        alert(`Erreur lors du chargement des joueurs: ${error.message}`);
     });
 });
 
