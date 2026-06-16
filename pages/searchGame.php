ea47b8b7f3b2d2e68b35d490d79a87636e775d00
flexult <touma.leandre.72@gmail.com>
2026-06-16 19:57:58 +0000

    patch 2

--- pages/searchGame.php
+++ pages/searchGame.php
@@ -5,20 +5,12 @@
 Permet un questionement approfondi de la db
 */
 
-// Vérifie que la db soir correctement inktialisée
-require_once __DIR__ . '/../config.php';
-try {
-    init_db();
-} catch (Throwable $error) {
-    echo '<link rel="stylesheet" href="/css/main.css">';
-    echo '<div class="error">' . htmlspecialchars($error->getMessage(), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . '</div>';
-    exit;
-}
-
 // Obtenir la liste des leaders depuis /datas.json
 $datas = file_get_contents(__DIR__ . '/../datas.json');
-$decoded_datas = json_decode($datas);
-$leader_names = $decoded_datas->leaders;
+$decoded_datas = json_decode($datas, true);
+$leader_names = is_array($decoded_datas['leaders'] ?? null)
+    ? $decoded_datas['leaders']
+    : [];
 
 // Élement DOM
 function leader_select(array $leader_names, string $name, string $id): string {
@@ -44,24 +36,6 @@ function leader_select(array $leader_names, string $name, string $id): string {
 </head>
 <body>
     <button type="button" id="back-btn" class="btn btn2">BACK</button>
-    <?php/*
-    <form class="form">
-        <label for="leader1" class="label">Selectionner un leader</label>
-        <?= leader_select('leader1', 'select2') ?>
-        <input type="radio" name="winningLeader" id="l1won" value="l1won">
-        <label for="l1won" class="label">Parties gagnées par ce leader</label>
-        <br>
-        <label for="leader2" class="label">Selectionner un autre leader</label>
-        <?= leader_select('leader2', 'select3') ?>
-        <input type="radio" name="winningLeader" id="l2won" value="l2won">
-        <label for="l2won" class="label">Parties gagnées par ce leader</label>
-        <br>
-        <input type="radio" name="winningLeader" id="nobodyWon" value="nobodyWon" checked>
-        <label for="nobodyWon" class="label">Chercher indépendament du gagant</label>
-        <br><br><br>
-        <button type="submit" id="submit-btn" class="btn btn3">RECHERCHER</button>
-    </form>
-    */?>
     <form class="form">
         <span class="text">Rechercher les</span>
         <select name="result" id="select1">
