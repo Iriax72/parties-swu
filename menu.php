b72f82714d2cfd9736a5376e115fa2632bda2641
flexult <touma.leandre.72@gmail.com>
2026-08-03 16:30:04 +0000

    patch

--- menu.php
+++ menu.php
@@ -12,7 +12,7 @@
     <a class="btn" href="/pages/addGame.php" id="addGameBtn">AJOUTER UNE PARTIE</a>
     <button class="btn" type="button" id="leaders-winrate-btn">CLASSEMENT DES LEADERS PAR WINRATE</button>
     <button class="btn" type="button" id="players-winrate-btn">CLASSEMENT DES JOUEURS PAR WINRATE</button>
-    <a class="btn" href="/pages/searchGames.php" id="search-games-btn">RECHERCHER DES PARTIES</a>
+    <a class="btn" href="/pages/searchGame.php" id="search-games-btn">RECHERCHER DES PARTIES</a>
     <button class="btn" type="button" id="see-decks-btn">VOIR LES DECKS</button>
     <a class="btn" href="/pages/addDeck.php" id="add-deck-btn">AJOUTER UN DECK</a>
 </body>
