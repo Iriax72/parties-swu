7b02f68d2f64eb60d403751d5b146666fc23b442
flexult <touma.leandre.72@gmail.com>
2026-08-13 18:57:39 +0000

    debug

--- api.php
+++ api.php
@@ -174,6 +174,8 @@ switch ($action) {
         $winning_deck = isset($_REQUEST['winning_deck']) && is_numeric($_REQUEST['winning_deck']) ? (int) $_REQUEST['winning_deck'] : null;
         $last_version_only = in_array($_REQUEST['last_version_only'], [0, 1]) ? $_REQUEST['last_version_only'] : 0;
 
+        echo "<script>alert(deck1: $deck1\ndeck2: $deck2\nwinning_deck: $winning_deck\nlast_version_only: $last_version_only);</script>";
+
         /*
         if (!$historical) {
             try {
